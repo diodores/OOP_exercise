c34166cdb334018f3cf421b89a6bea159eaf8879
diodores <s9634898702@gmail.com>
2021-08-30 23:14:23 +0300

    Проверена работа всех методов и функций

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ class Student:
 
     def rate_lecturer(self, lecturer, course, rate):
         """Ставим оценку лектору"""
-
-        mylist = {}
-        if (isinstance(lecturer, Lecturer) and course in self.courses_in_progress and
-                course in lecturer.courses_attached):
-            mylist[course] = rate
-            lecturer.assessment_lecturer.append(mylist)
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [rate]
+            else:
+                lecturer.grades[course] = [rate]
         else:
             return 'Ошибка'
 
     def average_rating(self):
         """Возвращает средний балл"""
         mylist = []
-        for i in self.grades.values():
-            mylist.append(i)
+        for grade in self.grades.values():
+            for i in grade:
+                mylist.append(i)
         average_rating = sum(mylist) / len(mylist)
         return average_rating
 
@@ -61,14 +61,15 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.assessment_lecturer = []
+        # self.assessment_lecturer = []
+        self.grades = {}
 
     def average_rating(self):
         """Возвращает средний балл"""
         mylist = []
-        for i in self.assessment_lecturer:
-            for score in i.values():
-                mylist.append(score)
+        for grade in self.grades.values():
+            for i in grade:
+                mylist.append(i)
         average_rating = sum(mylist) / len(mylist)
         return average_rating
 
@@ -107,28 +108,50 @@ class Reviewer(Mentor):
         return res
 
 
-student = Student('Dmitrij', 'Sazhin', 'man')
-student.courses_in_progress += ['python']
-lectur = Lecturer('Oleg', 'Bulygin')
-lectur.courses_attached += ['python']
-student.rate_lecturer(lectur, 'python', 10)
-a = Student('Q', 'O', 'b')
-a.courses_in_progress = ['python']
-a.rate_lecturer(lectur, 'python', 9)
-lectur.average_rating()
-q = Lecturer('w', 's')
-q.courses_attached = ['python']
-student.rate_lecturer(q, 'python', 7)
-print(q > lectur)
-print(student > lectur)
-student.grades['p']=8
-student.grades['py']=8
-a.grades['p']= 9
-a.grades['py']= 10
-print(a < lectur)
-print(student.grades)
-print(student.average_rating())
-g = Reviewer('Alex', 'Shpak')
-print(g)
-print(q)
-print(student)
+student_1 = Student('Иван', 'Иванов', 'муж')
+student_1.finished_courses += ['JS']
+student_1.courses_in_progress += ['python', 'git']
+student_2 = Student('Мария', 'Сидорова', 'жен')
+student_2.finished_courses += ['js']
+student_2.courses_in_progress += ['python', 'git']
+lecturer_1 = Lecturer('Алексей', 'Федорович')
+lecturer_1.courses_attached += ['python', 'git', 'js']
+lecturer_2 = Lecturer('Андрей', 'Васильевич')
+lecturer_2.courses_attached += ['python', 'git', 'js']
+reviewer_1 = Reviewer('Дмитрий', 'Анатольевич')
+reviewer_1.courses_attached += ['python', 'git', 'js']
+reviewer_2 = Reviewer('Владимир' , 'Владимирович')
+reviewer_2.courses_attached += ['python', 'git', 'js']
+student_1.rate_lecturer(lecturer_1, 'python', 9)
+student_1.rate_lecturer(lecturer_1, 'git', 10)
+student_2.rate_lecturer(lecturer_1, 'python', 7)
+student_2.rate_lecturer(lecturer_1, 'git', 9)
+student_1.rate_lecturer(lecturer_2, 'python', 6)
+student_1.rate_lecturer(lecturer_2, 'git', 8)
+student_2.rate_lecturer(lecturer_2, 'python', 7)
+student_2.rate_lecturer(lecturer_2, 'git', 9)
+reviewer_1.rate_hw(student_1, 'python', 6)
+reviewer_1.rate_hw(student_2, 'python', 4)
+reviewer_1.rate_hw(student_1, 'git', 8)
+reviewer_1.rate_hw(student_2, 'git', 7)
+reviewer_2.rate_hw(student_2, 'python', 10)
+reviewer_2.rate_hw(student_2, 'git', 7)
+reviewer_2.rate_hw(student_1, 'python', 10)
+reviewer_2.rate_hw(student_1, 'git', 7)
+student_1.average_rating()
+student_2.average_rating()
+print(student_1 < student_2)
+print()
+print(f'{student_1}\n')
+print(student_2)
+print()
+print(lecturer_1.average_rating())
+print(lecturer_2.average_rating())
+print()
+print(lecturer_2 < lecturer_1)
+print()
+print(f'{lecturer_1}\n')
+print(f'{lecturer_1}\n')
+print(f'{reviewer_1}\n')
+print(reviewer_2)
+
